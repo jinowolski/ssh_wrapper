11c49620378d0f6cd3d5087dfabbaf687db3e68b
Jan Inowolski <jinowolski@gmail.com>
2016-03-05 14:33:26 +0100

    Add "--" before logged message

--- ssh_wrapper.sh
+++ ssh_wrapper.sh
@@ -13,20 +13,20 @@ EXECUTABLE_DIR=$(dirname "$EXECUTABLE")
 
 if [ "$EXECUTABLE_DIR" != "$COMMANDS_DIR" ]; then
     >2& echo "Unknown command."
-    logger -p auth.warning "ssh_wrapper WARNING: Invalid directory. Caller tried command '$COMMAND'."
+    logger -p auth.warning -- "ssh_wrapper WARNING: Invalid directory. Caller tried command '$COMMAND'."
     exit 1
 fi
 
 if [ ! -x "$EXECUTABLE" ]; then
     if [ -f "$EXECUTABLE" ]; then
-        logger -p auth.info "ssh_wrapper INFO: Command '$COMMAND' exists, but cannot be executed."
+        logger -p auth.info -- "ssh_wrapper INFO: Command '$COMMAND' exists, but cannot be executed."
         >2& echo "This command exists, but cannot be executed."
     else
-        logger -p auth.info "ssh_wrapper INFO: Unknown command '$COMMAND'."
+        logger -p auth.info -- "ssh_wrapper INFO: Unknown command '$COMMAND'."
         >2& echo "Unknown command."
     fi
     exit 1
 fi
 
-logger -p auth.info "ssh_wrapper INFO: Executing command '$COMMAND' with args '$@'."
+logger -p auth.info -- "ssh_wrapper INFO: Executing command '$COMMAND' with args '$@'."
 $EXECUTABLE "$@"
